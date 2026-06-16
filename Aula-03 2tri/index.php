231ea60e602f3c98fefda41af502e9716da30a99
Guilherme Quemelli <guilherme.qfontana@aluno.edu.es.gov.br>
2026-06-16 11:07:45 -0300

    Create index.php

---
+++ Aula-03 2tri/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html lang "pt-bt">
+    <head>
+        <meta charset="UTF-8">
+        <title>aula 01 html </title>
+    </head>
+    <body>
+        <?php
+            echo "olá <br> Guilherme";
+        ?>
+        
+    </body>
+</html>
