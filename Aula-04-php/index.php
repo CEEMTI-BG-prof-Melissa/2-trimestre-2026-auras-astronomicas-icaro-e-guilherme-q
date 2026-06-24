b38e3d6902ba7ab78fac84efcb1be9d5a8fef0d1
Ícaro Corteletti Monteiro <icaro.cmonteiro@aluno.edu.es.gov.br>
2026-06-24 11:01:05 -0300

    Create index.php

---
+++ Aula-04-php/index.php
@@ -0,0 +1,17 @@
+<DOCTYPE HTML>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Aula 02</title>
+    </head>
+    <body>
+        <?php 
+        $nome = "icaro";
+        $idade = 17;
+
+        echo"Meu nome é " .$nome. " e tenho " .$idade. " Anos";
+
+        ?>
+    </body>
+
+</html>
